2b667c006ac9dea36127bee652d114e48e9ace5f
Ronyonka <rononyonka@gmail.com>
2019-02-19 09:01:24 +0300

    changes to db

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    BASE_URL = 'http://quotes.stormconsultancy.co.uk/{}.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ron:3159@localhost/rblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+   
+    pass
+
+class DevConfig(Config):
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
